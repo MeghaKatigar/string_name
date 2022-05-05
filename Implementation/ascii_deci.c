6a21765df825f94c8656e41c59a50e064871799c
Megha katigar <98883965+MeghaKatigar@users.noreply.github.com>
2022-05-05 14:26:32 +0530

    Update ascii_deci.c

--- Implementation/ascii_deci.c
+++ Implementation/ascii_deci.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include "function.c"
+#include "function.h"
 int main(){
 char_name Char;
 char name[20];
@@ -11,5 +11,16 @@ scanf("%s",name);
 int sum=char_ascii(Char, name);
 printf("%d\n",sum);
 return 0;
+}
+int char_ascii(char_name character, char *p)
+{
+//union Character character;
+int Sum=0;
+while(*p!='\0')
+{
+  character.ch=*p++;
+  Sum+=character.integer;
+}
+return Sum;
 }
 
