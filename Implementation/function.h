68093b971699d76b3ec7ecc00a56c1f4fe0d8ea8
Megha katigar <98883965+MeghaKatigar@users.noreply.github.com>
2022-05-05 14:26:27 +0530

    Update function.h

--- Implementation/function.h
+++ Implementation/function.h
@@ -1,6 +1,6 @@
 /**
  * @file function.h
- * @author Megha Katigar
+ * @author Megha Katigar (meghakatigar@gmail.com)
  * @brief 
  * @version 0.1
  * @date 2022-05-05
@@ -8,24 +8,12 @@
  * @copyright Copyright (c) 2022
  * 
  */
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
 typedef union{
 char ch;
 uint8_t  integer;
 }char_name;
 
-
-int char_ascii(char_name character, char *p)
-{
-//union Character character;
-int Sum=0;
-while(*p!='\0')
-{
-  character.ch=*p++;
-  Sum+=character.integer;
-}
-return Sum;
-}
+int char_ascii(char_name character, char *p);
+#endif
